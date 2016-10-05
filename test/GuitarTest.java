6a36cb4c670f2615408c77fcb32e597f44ee6648
Gearoid D. Kearney <t00137482@365s.ittralee.ie>
2016-10-05 12:35:41 +0100

    Initial commit

---
+++ test/GuitarTest.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by t00137482 on 05/10/2016.
+ */
+public class GuitarTest {
+    @Test
+    public void getSerialNumber() throws Exception {
+
+    }
+
+    @Test
+    public void getPrice() throws Exception {
+
+    }
+
+    @Test
+    public void setPrice() throws Exception {
+
+    }
+
+    @Test
+    public void getBuilder() throws Exception {
+
+    }
+
+    @Test
+    public void getModel() throws Exception {
+
+    }
+
+    @Test
+    public void getType() throws Exception {
+
+    }
+
+    @Test
+    public void getBackWood() throws Exception {
+
+    }
+
+    @Test
+    public void getTopWood() throws Exception {
+
+    }
+
+}
